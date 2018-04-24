0105760876f76710d54a7a22a0eeffcb7794255f
s3blag <226173@student.pwr.edu.pl>
2018-04-25 01:26:33 +0200

    Add ASP.NET MVC

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,50 @@
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMessage("qwerty@qwerty.qwe", model.Subject, $"From: {model.Name} ({model.Email}), Message: {model.Message}");
+                ViewBag.UserMessage = "Email Sent";
+                ModelState.Clear();
+            }
+            else
+            {
+
+            }
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About.";
+            return View();
+        }
+    }
+}
--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using DutchTreat.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,13 +11,25 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IMailService, NullMailService>();
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDefaultFiles();
+            if(env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
             app.UseStaticFiles();
+
+            app.UseMvc(cfg =>
+            {
+                cfg.MapRoute("Default",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "App", Action = "Index" });
+            });
         }
     }
 }
