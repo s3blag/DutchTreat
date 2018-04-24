c1568aed2666d2f2dcc07edb0f0b29bce78bb217
s3blag <226173@student.pwr.edu.pl>
2018-04-24 02:31:46 +0200

    Getting Started with HTML, CSS, and JS

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    let x = 0;
+    let s = "";
+
+    console.log("Hello World");
+
+    let theForm = $("#theForm");
+    theForm.hide();
+
+    let button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    let productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    let $loginToggle = $("#loginToggle");
+    let $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.slideToggle(200);
+    });
+});
+
+
