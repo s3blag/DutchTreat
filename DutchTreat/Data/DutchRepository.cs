05e6c75eb316148f33e6a7a5b76d76f514074c4d
s3blag <226173@student.pwr.edu.pl>
2018-04-25 18:57:35 +0200

    Improve logging

--- DutchTreat/Data/DutchRepository.cs
+++ DutchTreat/Data/DutchRepository.cs
@@ -1,4 +1,6 @@
 ﻿using DutchTreat.Data.Entities;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,29 +9,58 @@ namespace DutchTreat.Data
     public class DutchRepository : IDutchRepository
     {
         private readonly DutchContext _ctx;
+        private readonly ILogger<DutchRepository> _logger;
 
-        public DutchRepository(DutchContext ctx)
+        public DutchRepository(DutchContext ctx, ILogger<DutchRepository> logger)
         {
             _ctx = ctx;
+            _logger = logger;
         }
 
         public IEnumerable<Product> GetAllProducts()
         {
-            return _ctx.Products
-                       .OrderBy(p => p.Title)
-                       .ToList();
+            try
+            {
+                
+                return _ctx.Products
+                           .OrderBy(p => p.Title)
+                           .ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get all products: {ex}");
+                return null;
+            }
+           
         }
 
         public IEnumerable<Product> GetProductsByCategory(string category)
         {
-            return _ctx.Products
-                       .Where(p => p.Category == category)
-                       .ToList();
+            try
+            {
+                return _ctx.Products
+                           .Where(p => p.Category == category)
+                           .ToList();
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Failed to get products by category: {ex}");
+                return null;
+            }
+           
         }
 
         public bool SaveAll()
         {
-            return _ctx.SaveChanges() > 0;   
+            try
+            {
+                return _ctx.SaveChanges() > 0;
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Failed to save changes: {ex}");
+                return false;
+            }
         }
 
     }
